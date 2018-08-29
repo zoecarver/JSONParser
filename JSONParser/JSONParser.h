861ad8933d42cf25bbec0ddee6051a48c7f26c94
zoe <z.zoelec2@gmail.com>
2018-08-29 12:11:28 -0700

    Initial Commit

---
+++ JSONParser/JSONParser.h
@@ -0,0 +1,19 @@
+//
+//  JSONParser.h
+//  JSONParser
+//
+//  Created by Zoe IAMZOE.io on 8/29/18.
+//  Copyright © 2018 Zoe IAMZOE.io. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for JSONParser.
+FOUNDATION_EXPORT double JSONParserVersionNumber;
+
+//! Project version string for JSONParser.
+FOUNDATION_EXPORT const unsigned char JSONParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSONParser/PublicHeader.h>
+
+
